d90b3276525b2803f2537e9062c63242c7aec1d1
Wwd-2024 <9divinetime9@gmail.com>
2024-06-08 14:30:15 +0530

    Episode 05

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,42 @@
-import data from "../utils/mockData.json";
+import Shimmer from "./Shimmer";
 import RestaurantCard from "./RestaurantCard";
+import {useState, useEffect} from 'react';
 
-const resData = data;
 const Body = () => {
+
+    let [listData, setListData] = useState([]);
+
+    useEffect(()=> {
+        fetchData();
+    },[]);  
+
+
+    const fetchData = async () => { 
+
+        const liveData = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.96340&lng=77.58550&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+
+        const json =  await liveData.json();
+
+         const newResList  = json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+
+        setListData(newResList);
+    }
+
+   
+    if(listData.length === 0){
+        return <Shimmer />
+    }
+
     return (
         <div className="body">
             <div className="toolbar">
-            <button onClick={()=>console.log("d")}>Top Rated Restaurants</button>
+            <button onClick={()=> {
+            const filteredData = listData.filter(res=> res.info.avgRating > 4.3);
+            setListData(filteredData);
+           
+        }
+            
+            }>Top Rated Restaurants</button>
             <div className="searchbar">
             <input type="text" placeholder="Search.." name="search"/>
             <button type="submit">{<i className="fa fa-search"></i>}</button>
@@ -15,7 +45,7 @@ const Body = () => {
            
             <div className="res-container">
             {
-                resData.map((restaurant) =>(
+                listData.map((restaurant) =>(
                     <RestaurantCard key={restaurant.info.id} resData={restaurant}/>
                 )
                 )
